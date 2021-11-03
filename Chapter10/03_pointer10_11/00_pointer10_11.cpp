9d4268826e249720b35bc12b87e5edab1a3645f0
lole-elol <loleider@gmx.de>
2021-11-03 11:06:44 +0100

    Chapter 10 ex 11

---
+++ Chapter10/03_pointer10_11/00_pointer10_11.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iterator>
+
+// iterates over array, returns pointer to found value
+int *find(int *begin, int *end, int value)
+{
+
+    for (int *ptr{begin}; ptr != end; ++ptr)
+    {
+        if (*ptr == value)
+        {
+            return ptr;
+        }
+    }
+
+    return end;
+}
+
+int main()
+{
+    int arr[]{2, 5, 4, 10, 8, 20, 16, 40};
+
+    // Search for the first element with value 20.
+    int *found{find(std::begin(arr), std::end(arr), 20)};
+
+    // If an element with value 20 was found, print it.
+    if (found != std::end(arr))
+    {
+        std::cout << *found << '\n';
+    }
+
+    return 0;
+}
