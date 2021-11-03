7beb22b2248d5dc1f23411ac49560d8ad6e6573c
lole-elol <loleider@gmx.de>
2021-11-03 21:42:05 +0100

    Chapter 10 Summary Question4

---
+++ Chapter10/06_Summary10_x/03_QuestioFour10_x.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void printLikeC(char *ptr, int length)
+{
+
+    for (int i{0}; i <= length - 1; ++i)
+    {
+        std::cout << *ptr << " ";
+        ++ptr;
+    }
+}
+
+// faster way
+void printLikeCFAST(const char *str)
+{
+    // While we haven't encountered a null terminator
+    while (*str != '\0')
+    {
+        // print the current character
+        std::cout << *str;
+
+        // and point str at the next character
+        ++str;
+    }
+}
+
+int main()
+{
+
+    char my_string[]{"Hello, world!"};
+    int length{sizeof(my_string)};
+    printLikeC(my_string, length);
+    printLikeCFAST(my_string);
+
+    return 0;
+}
